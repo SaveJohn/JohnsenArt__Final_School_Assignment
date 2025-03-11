0429b320535647f1bd8d56dce47b15f99e7a539b
SaveJohn <sabinavedvikjohnsen@gmail.com>
2025-03-11 14:01:23 +0100

    Set up for logging, serilog using eris for json

--- JoArtAPI/Program.cs
+++ JoArtAPI/Program.cs
@@ -1,5 +1,6 @@
 using JoArtDataLayer;
 using Microsoft.EntityFrameworkCore;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,11 +10,16 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 // Database context
 builder.Services.AddDbContext<JoArtDbContext>(options =>
     options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"),
         ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("DefaultConnection"))));
 
+// Logging
+builder.Host.UseSerilog((context, services, configuration) => 
+    configuration.ReadFrom.Configuration(context.Configuration)
+);
 
 var app = builder.Build();
 
