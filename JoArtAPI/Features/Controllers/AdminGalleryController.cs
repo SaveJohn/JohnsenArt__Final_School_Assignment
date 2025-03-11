1c8ce8a57ef7bcf38cb1fcfbf346909c046d33a0
SaveJohn <sabinavedvikjohnsen@gmail.com>
2025-03-11 14:45:55 +0100

    Added empty controller classes, and started on repository for public gallery

--- JoArtAPI/Features/Controllers/AdminGalleryController.cs
+++ JoArtAPI/Features/Controllers/AdminGalleryController.cs
@@ -1,10 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace JohnsenArtAPI.Controllers;
 
-[Route("api/[controller]")]
-[ApiController]
-public class AdminGalleryController
+[Authorize]
+[Route("admin/api/[controller]")]
+public class AdminGalleryController : ControllerBase 
 {
     
 }
---
+++ JoArtDataLayer/Repositories/PublicGalleryRepository.cs
@@ -0,0 +1,34 @@
+﻿using JoArtClassLib;
+using JoArtClassLib.Art;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace JoArtDataLayer.Repositories;
+
+public class PublicGalleryRepository
+{
+    private readonly JoArtDbContext _dbContext;
+    private readonly ILogger<PublicGalleryRepository> _logger;
+
+    public PublicGalleryRepository(JoArtDbContext dbContext, ILogger<PublicGalleryRepository> logger)
+    {
+        _dbContext = dbContext;
+        _logger = logger;
+    }
+    
+    // Get all artworks
+    public async Task<List<Artwork>> GetAllArtworksAsync()
+    {
+        return await _dbContext.Artworks
+            .Include(a => a.Images) 
+            .ToListAsync();
+    }
+    
+    // Get artwork by ID
+    public async Task<List<Artwork>> GetArtworkByIdAsync()
+    {
+        throw new NotImplementedException();
+    }
+    
+    // Filter
+}
