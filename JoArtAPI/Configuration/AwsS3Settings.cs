0c77ccf15b02a76621ee1a811e93df6311dfc548
SaveJohn <sabinavedvikjohnsen@gmail.com>
2025-03-16 02:29:54 +0100

    An image can now be uploaded to S3 bucket. Need a lot more work; this is just an image and not the full Artwork object.

--- JoArtAPI/Configuration/AwsS3Settings.cs
+++ JoArtAPI/Configuration/AwsS3Settings.cs
@@ -3,4 +3,5 @@
 public class AwsS3Settings
 {
     public int FileExpireInSeconds { get; set; }
+    public string BucketName { get; set; }
 }
--- JoArtAPI/Features/Controllers/AdminGalleryController.cs
+++ JoArtAPI/Features/Controllers/AdminGalleryController.cs
@@ -1,4 +1,5 @@
-﻿using JohnsenArtAPI.Services.Interfaces;
+﻿using System.Net;
+using JohnsenArtAPI.Services.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -22,12 +23,36 @@ public class AdminGalleryController : ControllerBase
     // Get artwork
     
     // Upload artwork
-    [HttpPost]
-    public async Task<IResult> UploadArtwork([FromForm]IFormFile imageFile, string bucketName)
+    [HttpPost("upload")]
+    public async Task<IActionResult> UploadArtwork([FromForm] IFormFile imageFile)
     {
-        var response = await _adminGalleryService.UploadArtworkAsync(imageFile, bucketName);
-        return Results.Ok(response);
+        if (imageFile == null || imageFile.Length == 0)
+        {
+            _logger.LogWarning("UploadArtwork: No file provided.");
+            return BadRequest("File is required.");
+        }
+
+        try
+        {
+            var response = await _adminGalleryService.UploadArtworkAsync(imageFile);
+
+            if (response == HttpStatusCode.OK || response == HttpStatusCode.Created)
+            {
+                _logger.LogInformation("UploadArtwork: File {FileName} uploaded successfully.", imageFile.FileName);
+                return Ok(new { Message = "Upload successful", FileName = imageFile.FileName });
+            }
+
+            _logger.LogError("UploadArtwork: Upload failed for {FileName}, StatusCode: {StatusCode}", 
+                imageFile.FileName, response);
+            return StatusCode((int)response, "Upload failed.");
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "UploadArtwork: Error uploading {FileName}", imageFile?.FileName);
+            return StatusCode(500, "Internal server error.");
+        }
     }
+
     
     // Edit artwork 
     
