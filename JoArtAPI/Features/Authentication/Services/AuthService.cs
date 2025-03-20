53b80dffdcfc70cbf9ddd45127bf10a21cecb0d3
Sebastian Krøger Holmen <79469928+Horseborn@users.noreply.github.com>
2025-03-20 16:28:55 +0100

    Okay .. it was extremely naive to think it would be fast. So migrations are finally added, they are not done within docker container but rather locally, we can transfer migrations later. A hashed value representing "password" is now added to the Database, when logging in the value is retrieved from database, and jwt key is delivered.

--- JoArtAPI/Features/Authentication/Services/AuthService.cs
+++ JoArtAPI/Features/Authentication/Services/AuthService.cs
@@ -1,5 +1,8 @@
 ﻿using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
+using BCrypt.Net;
+using JoArtDataLayer;
+using Microsoft.EntityFrameworkCore;
 using System.Text;
 using JohnsenArtAPI.Features.Authentication.Interfaces;
 using JohnsenArtAPI.Features.Authentication.Models;
@@ -11,20 +14,35 @@ namespace JohnsenArtAPI.Features.Authentication.Services;
 public class AuthService : IAuthService
 {
     private readonly IConfiguration _configuration;
+    private readonly JoArtDbContext _dbContext;
+    private readonly ILogger<AuthService> _logger;
 
-    public AuthService(IConfiguration configuration)
+    public AuthService(IConfiguration configuration, JoArtDbContext dbContext, ILogger<AuthService> logger)
     {
+        _dbContext = dbContext;
+        _logger = logger;
         _configuration = configuration;
     }
 
     public async Task<AuthResponse> LoginAsync(LoginRequest loginRequest)
     {
-        if (loginRequest.Email != "admin" || loginRequest.Password != "password")
+        var admin = await _dbContext.Admins
+            .FirstOrDefaultAsync(a => a.Email == loginRequest.Email);
+
+        if (admin == null)
         {
             return null;
         }
 
         var user = new UserDTO { Email = loginRequest.Email, AdminId = 1 };
+        bool isPasswordValid = BCrypt.Net.BCrypt.Verify(loginRequest.Password, admin.HashedPassword);
+
+        var user = new UserDTO
+        {
+            AdminId = admin.AdminId,
+            Email = admin.Email,
+            Name = admin.Name,
+        };
         var token = GenerateJwtToken(user);
 
         return new AuthResponse { Token = token };
@@ -32,7 +50,8 @@ public class AuthService : IAuthService
 
     public string GenerateJwtToken(UserDTO user)
     {
-        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]!));
+        var keyBytes = Convert.FromBase64String(_configuration["JWT:Key"]!);
+        var key = new SymmetricSecurityKey(keyBytes);
         var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
 
         var claims = new[]
---
+++ JoArtDataLayer/DbContext/JoArtDbContextFactory.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace JoArtDataLayer;
+
+public class JoArtDbContextFactory : IDesignTimeDbContextFactory<JoArtDbContext>
+{
+    public JoArtDbContext CreateDbContext(string[] args)
+        // This class creates an instance of JoArtDbContext which is necessary in a class library without a program.cs
+    {
+        var jsonConfiguration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.DesignTime.json", optional: false, reloadOnChange: true)
+            .Build();
+
+
+        string connectionString = jsonConfiguration.GetConnectionString("DefaultConnection")
+                                  ?? throw new InvalidOperationException("Connection string was not found.");
+
+        var optionsBuilder = new DbContextOptionsBuilder<JoArtDbContext>();
+        optionsBuilder.UseMySql(connectionString, new MySqlServerVersion(new Version(8, 0, 0)));
+
+
+        return new JoArtDbContext(optionsBuilder.Options);
+    }
+}
