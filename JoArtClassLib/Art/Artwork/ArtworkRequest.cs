6758cb79563c8c2b50443a4f7ea9d14239e9b448
Sabina Johnsen <sabinavedvikjohnsen@gmail.com>
2025-03-31 00:11:16 +0200

    UploadArtwork.razor progress, need to get the submit button to work + minor tweaking on GUI

--- JoArtClassLib/Art/Artwork/ArtworkRequest.cs
+++ JoArtClassLib/Art/Artwork/ArtworkRequest.cs
@@ -1,14 +1,25 @@
 ﻿namespace JoArtClassLib.Art;
 
+using System.ComponentModel.DataAnnotations;
+
 public class ArtworkRequest
 {
-    
+    [Required(ErrorMessage = "Tittel er påkrevd.")]
     public string Title { get; set; }
+
     public string? Description { get; set; }
+
     public string Artist { get; set; } = "Bjarne Johnsen";
+
+    [Range(0, double.MaxValue, ErrorMessage = "Pris må være et positivt tall.")]
     public decimal? Price { get; set; }
+
+    [Range(0, double.MaxValue, ErrorMessage = "Høyde må være et positivt tall.")]
     public double? HeightDimension { get; set; }
+
+    [Range(0, double.MaxValue, ErrorMessage = "Bredde må være et positivt tall.")]
     public double? WidthDimension { get; set; }
+
     public bool ForSale { get; set; }
     
     public virtual List<ImageRequest> Images { get; set; } = new();
--- JoArtClassLib/Art/ArtworkImage/ImageRequest.cs
+++ JoArtClassLib/Art/ArtworkImage/ImageRequest.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.AspNetCore.Http;
-
-namespace JoArtClassLib.Art;
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Http;
 
 public class ImageRequest
 {
     public int? Id { get; set; }
+
+    [Required(ErrorMessage = "Bilde er påkrevd.")]
     public IFormFile? ImageFile { get; set; }
+
     public bool IsWallPreview { get; set; }
 }
