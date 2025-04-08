f08f2d83d2f70659517db8323591555213a92858
Sebastian Krøger Holmen <79469928+Horseborn@users.noreply.github.com>
2025-04-08 19:31:14 +0200

    Payment intent endpoint created, can be hit using an artwork instance with a price and forsale = true, will return a valid clientsecret.

---
+++ JoArtAPI/Features/Payments/Controllers/StripeController.cs
@@ -0,0 +1,39 @@
+﻿using JohnsenArtAPI.Features.Gallery.Common.Interfaces;
+using JohnsenArtAPI.Features.Payments.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JohnsenArtAPI.Features.Payments.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class StripeController : ControllerBase
+{
+    private readonly IStripeService _stripeService;
+    private readonly IGalleryService _galleryService;
+    private readonly ILogger<StripeController> _logger;
+
+    public StripeController(IStripeService stripeService,
+        IGalleryService galleryService,
+        ILogger<StripeController> logger)
+    {
+        _stripeService = stripeService;
+        _galleryService = galleryService;
+        _logger = logger;
+    }
+
+    [HttpPost("create-intent/{artworkId}")]
+    public async Task<IActionResult> CreatePaymentIntent(int artworkId)
+    {
+        _logger.LogInformation($"Creating payment intent for {artworkId}");
+
+        var artwork = await _galleryService.GetArtworkByIdAsync(artworkId);
+
+        if (artwork == null || artwork.Price == null || artwork.ForSale == false)
+        {
+            return BadRequest("Chosen artwork is not available for sale.");
+        }
+
+        var intent = await _stripeService.CreatePaymentIntentAsync(artwork);
+        return Ok(new { clientSecret = intent.ClientSecret });
+    }
+}
