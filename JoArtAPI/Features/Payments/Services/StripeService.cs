94d44881d1d78a62b07fe9f2b19fc14e5d3af4a7
Sebastian Krøger Holmen <79469928+Horseborn@users.noreply.github.com>
2025-04-08 19:29:52 +0200

    Stripe backend functionality started. Added relevant folders according to project structure. Service implemented.

---
+++ JoArtAPI/Features/Payments/Services/StripeService.cs
@@ -0,0 +1,37 @@
+﻿using JoArtClassLib.Art;
+using JohnsenArtAPI.Features.Payments.Interfaces;
+using Stripe;
+
+namespace JohnsenArtAPI.Features.Payments.Services;
+
+public class StripeService : IStripeService
+{
+    private readonly IConfiguration _config;
+    private readonly ILogger<StripeService> _logger;
+
+    public StripeService(IConfiguration config, ILogger<StripeService> logger)
+    {
+        _config = config;
+        _logger = logger;
+    }
+
+    public async Task<PaymentIntent> CreatePaymentIntentAsync(ArtworkResponse artwork)
+    {
+        var options = new PaymentIntentCreateOptions
+        {
+            Amount = (long)((artwork.Price ?? 0) * 100),
+            Currency = "nok",
+            Metadata = new Dictionary<string, string>
+            {
+                { "artworkId", artwork.Id.ToString() },
+                { "title", artwork.Title },
+            }
+        };
+
+        var service = new PaymentIntentService();
+        var intent = await service.CreateAsync(options);
+        _logger.LogInformation("Intent payment created with id {Id}", intent.Id);
+
+        return intent;
+    }
+}
