4619938aef170c568a8a93c8933770b5f16c4d54
Sabina Johnsen <sabinavedvikjohnsen@gmail.com>
2025-04-15 00:44:41 +0200

    Deleted Order models that won't be used and created a BuyerInfo.cs model instead (for buyer info that will be added to the mail sent to seller, and makes sure buyer gets a mail too?

---
+++ JoArtClassLib/Payment/BuyerInfo.cs
@@ -0,0 +1,11 @@
+﻿namespace JoArtClassLib.Payment;
+
+public class BuyerInfo
+{
+    public string FullName { get; set; } = "";
+    public string Email { get; set; } = "";
+    public string PhoneNumber { get; set; } = "";
+    public string AddressLine { get; set; } = "";
+    public string City { get; set; } = "";
+    public string PostalCode { get; set; } = "";
+}
