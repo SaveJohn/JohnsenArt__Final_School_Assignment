768737db9b1b3933c83f25479834934262d0fd00
Sebastian Krøger Holmen <79469928+Horseborn@users.noreply.github.com>
2025-04-15 21:07:07 +0200

    Added email DTO and IEmailService interface.

---
+++ JoArtAPI/Features/Contact/DTO/EmailRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace JohnsenArtAPI.Features.Contact.DTO;
+
+public class EmailRequest
+{
+    public string Name { get; set; } = string.Empty;
+    public string FromEmail { get; set; } = string.Empty;
+    public string Message { get; set; } = string.Empty;
+}
