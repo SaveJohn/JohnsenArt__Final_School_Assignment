bf87c681fc755c86a8fd43dc9c2fd0b281d50514
Sabina Johnsen <sabinavedvikjohnsen@gmail.com>
2025-04-16 17:23:28 +0200

    Checkout progress: Shipping part is done (unless improvement on shipping-costs-estimates are desired by our customer, cannot use Bring API without a registered business, so a simple estimate method is made, using a class holding const prices for small, medium and large artwork)

--- JoArtClassLib/Payment/BuyerInfo.cs
+++ JoArtClassLib/Payment/BuyerInfo.cs
@@ -5,6 +5,8 @@ public class BuyerInfo
     public string FullName { get; set; } = "";
     public string Email { get; set; } = "";
     public string PhoneNumber { get; set; } = "";
+    
+    public string DeliveryMethod { get; set; } = "";
     public string AddressLine { get; set; } = "";
     public string City { get; set; } = "";
     public string PostalCode { get; set; } = "";
---
+++ JoArtClassLib/Payment/ShippingCosts.cs
@@ -0,0 +1,8 @@
+﻿namespace JoArtClassLib.Payment;
+
+public class ShippingCosts
+{
+    public const decimal Small = 150m; 
+    public const decimal Medium = 250m;
+    public const decimal Large = 350m;
+}
