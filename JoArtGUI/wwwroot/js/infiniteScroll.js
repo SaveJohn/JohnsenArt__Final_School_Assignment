032b7e352c868efb0971e487c919e779296b820f
Sabina Johnsen <sabinavedvikjohnsen@gmail.com>
2025-04-21 23:33:53 +0200

    Added paging through "infinite scroll"

---
+++ JoArtGUI/wwwroot/js/infiniteScroll.js
@@ -0,0 +1,19 @@
+﻿export function observeElement(dotNetHelper, element) {
+    if (!element) {
+        console.warn("Element was null – will not observe.");
+        return;
+    }
+
+    console.log("Observer attached!");
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                console.log("Trigger reached - requesting next page...");
+                dotNetHelper.invokeMethodAsync('LoadMoreArtworks');
+            }
+        });
+    });
+
+    observer.observe(element);
+}
