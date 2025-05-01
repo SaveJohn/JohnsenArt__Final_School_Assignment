7a7fb1bda77e6f05806c216cc086a45db670f977
Sebastian Krøger Holmen <79469928+Horseborn@users.noreply.github.com>
2025-05-01 21:20:22 +0200

    Stripe config interface and stripe webhook simulation file added.

---
+++ JoArtAPI/Features/Payments/Interfaces/IStripeConfigProvider.cs
@@ -0,0 +1,8 @@
+﻿using JoArtClassLib.AwsSecrets;
+
+namespace JohnsenArtAPI.Features.Payments.Interfaces;
+
+public interface IStripeConfigProvider
+{
+    Task<StripeSecretConfig> GetStripeConfigAsync();
+}
