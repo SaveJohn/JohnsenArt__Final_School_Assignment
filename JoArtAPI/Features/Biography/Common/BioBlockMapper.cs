e7362645208a71a27a1b09d09ba0109d26268685
Sabina Johnsen <sabinavedvikjohnsen@gmail.com>
2025-08-23 17:47:41 +0200

    Bio Block Mapper

---
+++ JoArtAPI/Features/Biography/Common/BioBlockMapper.cs
@@ -0,0 +1,18 @@
+﻿using Profile = AutoMapper.Profile;
+using JoArtClassLib.About;
+
+namespace JohnsenArtAPI.Features.Biography.Common;
+
+public class BioBlockMapper : Profile
+{
+    public BioBlockMapper()
+    {
+        CreateMap<BioBlock, BioBlockRequest>().ReverseMap();
+        CreateMap<BioImage, BioImageRequest>().ReverseMap();
+        CreateMap<BioBlock, UpdateBioBlockRequest>().ReverseMap();
+        CreateMap<BioImage, UpdateBioImageRequest>().ReverseMap();
+        CreateMap<BioBlock, BioBlockResponse>().ReverseMap();
+        CreateMap<BioImage, BioImageResponse>().ReverseMap();
+    }
+    
+}
